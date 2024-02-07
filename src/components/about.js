81c20f40eab05dbeab27f059b31e7a59cfcbc5ab
Khadiza-Akter-Developer <khadizaakter44555@gmail.com>
2024-02-07 22:39:08 +0600

    about

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,15 @@ const About = () => {
         <p>
         Happiness is the fashion for everyone for every occasion.
 
-Synonymous with grace and effortless style, Happiness offers value fashion for Men, Women, Kids. Le Reve presents a complete wardrobe of uniquely crafted Ethnic Wear, Casuals, Edgy Denims, & Accessories inspired from the most contemporary fashion trends across the globe in an exciting mix of silhouettes, colors & styles.
+Synonymous with grace and effortless style, Happiness offers value fashion for Men, Women, Kids.  Happiness is the fashion for everyone for every occasion.
+
+Synonymous with grace and effortless style, Happiness offers value fashion for Men, Women, Kids. Happiness presents a complete wardrobe of uniquely crafted Ethnic Wear, Casuals, Edgy Denims, & Accessories inspired from the most contemporary fashion trends across the globe in an exciting mix of silhouettes, colors & styles.
+
+Happiness embodies the spirit of today’s youth that is playful, fashion & value conscious.
+
+At Happiness, customers can browse at convenience, and shop in a relaxed and pleasing atmosphere at all our stores. The use of space is carefully done to give a truly international retail ambience. Our continuous endeavor is to provide the consumers a delightful shopping experience through world-class ambience, customer service and clearly differentiated merchandising concepts. In a short period of time, our stores have become one of the most preferred shopping destinations in prime locations of Bangladesh & we are expanding aggressively.
+
+Happiness is part of REVE Tex Ltd, one of the members of REVE family; started its first retail store in 2009, today we have 19 stores at prime locations in Dhaka, Chattogram, Sylhet, Khulna, Rajshahi & Narayanganj. presents a complete wardrobe of uniquely crafted Ethnic Wear, Casuals, Edgy Denims, & Accessories inspired from the most contemporary fashion trends across the globe in an exciting mix of silhouettes, colors & styles.
 
 Happiness embodies the spirit of today’s youth that is playful, fashion & value conscious.
 
